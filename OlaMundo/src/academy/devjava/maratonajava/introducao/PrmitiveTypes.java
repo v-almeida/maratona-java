6a90ef291acdc7972084fb015966a16a3d6eff0c
v-almeida <almeida.vitor20000@gmail.com>
2026-03-20 21:44:03 -0300

    added more exemples

--- OlaMundo/src/academy/devjava/maratonajava/introducao/PrmitiveTypes.java
+++ OlaMundo/src/academy/devjava/maratonajava/introducao/PrmitiveTypes.java
@@ -3,6 +3,14 @@ package academy.devjava.maratonajava.introducao;
 public class PrmitiveTypes {
     public static void main(String[] args){
         int age = 10;
+        long numberLong = 1000000;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean isTrue = true;
+        boolean isFalse = false;
+        char character = 'F';
         System.out.println("age = " + age);
     }
 }
