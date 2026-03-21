a21d69f1f43337e7947c95941204e65d8b631e13
v-almeida <almeida.vitor20000@gmail.com>
2026-03-20 21:35:32 -0300

    int the first primitive type example

---
+++ OlaMundo/src/academy/devjava/maratonajava/introducao/PrmitiveTypes.java
@@ -0,0 +1,8 @@
+package academy.devjava.maratonajava.introducao;
+
+public class PrmitiveTypes {
+    public static void main(String[] args){
+        int age = 10;
+        System.out.println("age = " + age);
+    }
+}
